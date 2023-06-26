556945a32e37226bfae57f7deba8356d5ca8ad32
silas-inegbe <kingssilas18@gmail.com>
2023-06-26 14:16:23 +0100

    i created a folder for the frontend part

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+
+
+`;
+
+const Home = () => {
+    return ( 
+
+        <Container>
+            <h1>Hello world</h1>
+        </ Container>
+     );
+}
+ 
+export default Home;
